e805e9bd117c6a4c524728b46e347ef152b437a5
Ihor Stepanov <igor.stepanov2000@gmail.com>
2022-06-29 18:29:51 +0300

    expand examples

--- doc/examples/IMDB_classification.py
+++ doc/examples/IMDB_classification.py
@@ -1,9 +1,12 @@
 from ptic import pmi_tfidf_classifier as ptah
 import numpy as np
 import pandas as pd
+from sklearn import metrics
+import torch
+from ptic import neuro_ptic
 
 np.random.seed(42)
-path = "../datasets/"
+path = "doc/datasets/"
 
 # Data is taken from here
 # https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
@@ -11,7 +14,10 @@ data_raw = pd.read_csv(path + 'IMDB_Dataset.csv')
 indices = np.random.permutation(data_raw.index)
 data = data_raw.loc[indices]
 data = data_raw.sample(frac=1)
-data = data.replace(to_replace=['negative', 'positive'], value=[0, 1])
+labels_names = ['negative', 'positive']
+nl = len(labels_names)
+
+data = data.replace(to_replace=labels_names, value=[0, 1])
 
 idx = int(data.shape[0] * 0.1)
 test_data = data.iloc[:idx]
@@ -25,13 +31,26 @@ N = len(tokenized_texts)
 
 word2text_count = ptah.get_word_stat(tokenized_texts)
 words_pmis = ptah.create_pmi_dict(tokenized_texts, targets_train, min_count=5)
-results = ptah.classify_pmi_based(words_pmis, word2text_count, tokenized_test_texts, N)
-precision = np.sum( np.logical_and(results, targets_test) ) / np.sum(results)
-recall = np.sum( np.logical_and(results, targets_test) ) / np.sum(targets_test)
-F1 = 2 * (recall * precision)/(recall + precision)
-accuracy = (results == targets_test).mean()
 
-print("Accuracy: ", accuracy)
-print("Precision: ", precision)
-print("Recall: ", recall)
-print("F1: ", F1)
+words = set()
+for wpid in words_pmis:
+    words.update(words_pmis[wpid].keys())
+word2id = {w:id for id, w in enumerate(words)}
+lpw = len(words)
+
+X = neuro_ptic.get_pmi_vectors(words_pmis, word2text_count, word2id, tokenized_texts, N, lpw, nl)
+X_test = neuro_ptic.get_pmi_vectors(words_pmis, word2text_count, word2id, tokenized_test_texts, N, lpw, nl)
+Y = torch.from_numpy(targets_train)
+Y_test = torch.from_numpy(targets_test)
+dili_net = neuro_ptic.train(X=X, Y=Y, X_test=X_test, Y_test=Y_test, wc = lpw, nl = nl, batch_size=100, epochs=300)
+
+results = ptah.classify_pmi_based(words_pmis, word2text_count, tokenized_test_texts, N)
+
+results_net = neuro_ptic.get_dili_net_results(dili_net, words_pmis, word2id, word2text_count, tokenized_test_texts, N, lpw, nl)
+
+print('Results of the conventional ptic classifier:')
+print(metrics.classification_report(results, targets_test, digits=3, target_names=labels_names))
+
+print('Results of the neural network classifier:')
+print(metrics.classification_report(results_net, targets_test, digits=3, target_names=labels_names))
+
--- doc/examples/multi_classification_conferences.py
+++ doc/examples/multi_classification_conferences.py
@@ -2,9 +2,11 @@ from ptic import pmi_tfidf_classifier as ptah
 import numpy as np
 import pandas as pd
 from sklearn import metrics
+import torch
+from ptic import neuro_ptic
 
 np.random.seed(42)
-path = "../datasets/"
+path = "doc/datasets/"
 
 # Data is taken from here
 # https://raw.githubusercontent.com/susanli2016/NLP-with-Python/master/data/title_conference.csv
@@ -13,10 +15,12 @@ indices = np.random.permutation(data_raw.index)
 data = data_raw.loc[indices]
 data = data_raw.sample(frac=1)
 labels = data_raw.Conference.unique()
+labels_names = {str(labels[i]): i for i in range(len(labels))}
+nl = len(labels)
 
 data = data.replace(to_replace=labels, value=[0, 1, 2, 3, 4])
 
-idx = int(data.shape[0] * 0.1)
+idx = int(data.shape[0] * 0.2)
 test_data = data.iloc[:idx]
 train_data = data.iloc[idx:]
 targets_train = train_data["Conference"].values
@@ -27,8 +31,29 @@ tokenized_test_texts = ptah.tokenization(test_data, 'Title')
 N = len(tokenized_texts)
 
 word2text_count = ptah.get_word_stat(tokenized_texts)
-words_pmis = ptah.create_pmi_dict(tokenized_texts, targets_train, min_count=5)
+words_pmis = ptah.create_pmi_dict(tokenized_texts, targets_train, min_count=1)
+
+words = set()
+for wpid in words_pmis:
+    words.update(words_pmis[wpid].keys())
+word2id = {w:id for id, w in enumerate(words)}
+lpw = len(words)
+
 results = ptah.classify_pmi_based(words_pmis, word2text_count, tokenized_test_texts, N)
 
 
+X = neuro_ptic.get_pmi_vectors(words_pmis, word2text_count, word2id, tokenized_texts, N, lpw, nl)
+X_test = neuro_ptic.get_pmi_vectors(words_pmis, word2text_count, word2id, tokenized_test_texts, N, lpw, nl)
+Y = torch.from_numpy(targets_train)
+Y_test = torch.from_numpy(targets_test)
+dili_net = neuro_ptic.train(X=X, Y=Y, X_test=X_test, Y_test=Y_test, wc = lpw, nl = nl, batch_size=100, epochs=500)
+
+results = ptah.classify_pmi_based(words_pmis, word2text_count, tokenized_test_texts, N)
+
+results_net = neuro_ptic.get_dili_net_results(dili_net, words_pmis, word2id, word2text_count, tokenized_test_texts, N, lpw, nl)
+
+print('Results of the conventional ptic classifier:')
 print(metrics.classification_report(results, targets_test, digits=3, target_names=labels))
+
+print('Results of the neural network classifier:')
+print(metrics.classification_report(results_net, targets_test, digits=3, target_names=labels_names))
